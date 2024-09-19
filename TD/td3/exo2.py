2e1b2ed72d9589fef042ab8c6c23b7f5f711e4f2
Perceval00731 <fbthomas9@gmail.com>
2024-09-19 12:15:15 +0200

    exo2

---
+++ TD/td3/exo2.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3
+from os import *
+from sys import argv
+
+try:
+    if len(argv)<=1:
+        write(2,b"missing argument\n")
+        exit(1)
+    
+    #lire le fichier
+    fd=open(argv[1],O_RDONLY)
+    buffer=read(fd,256)
+    while len(buffer)>0:
+        write(1,buffer)
+        buffer=read(fd,256)
+    close(fd)
+
+except OSError as e:
+    print("system call error",e.strerror)
+    exit(1)
