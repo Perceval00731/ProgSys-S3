b4a77074e87ef597711988d2851f0a05465cc8bb
Perceval00731 <fbthomas9@gmail.com>
2024-09-27 09:46:39 +0200

    td4

--- TD/td4/td4.py
+++ TD/td4/td4.py
@@ -22,6 +22,12 @@ if pid == 0:  # Processus enfant
     os.close(w)  # Fermer le descripteur d'écriture
 else:  # Processus parent
     os.close(w)  # Fermer le descripteur d'écriture
+    os.waitpid(pid, 0)  # Attendre la fin du processus enfant
     sortie = os.read(r, 1024)  # Lire depuis le pipe
     os.close(r)  # Fermer le descripteur de lecture
     print(sortie.decode())  # Afficher le contenu lu
+
+# Lire et afficher le contenu du fichier
+with open(chemin_fichier, 'r') as file:
+    contenu = file.read()
+    print("Contenu du fichier:", contenu)
