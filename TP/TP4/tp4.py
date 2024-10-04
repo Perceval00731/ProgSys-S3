5e7efee0964e4625bf7eec8f2b87ee4d057d736e
Perceval00731 <fbthomas9@gmail.com>
2024-10-04 09:51:31 +0200

    tp4

--- TP/TP4/tp4.py
+++ TP/TP4/tp4.py
@@ -5,3 +5,8 @@ import sys
 try:
     fs=open(fSource, O_RDONLY)
     fd=open(fDest, O_WRONLY|O_CREAT|O_CREAT, 0o644)
+
+except OSError as e:
+    print("Erreur d'ouverture du fichier")
+    print(traceback.format_exc())
+    sys.exit(1)
